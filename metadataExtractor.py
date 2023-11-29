ee78f5b412697c433fefa4217689a8abafd5e9ec
ewanpedersenUVM <ejpeders@uvm.edu>
2023-11-29 12:44:26 -0500

    created basic metadata extractor

--- metadataExtractor.py
+++ metadataExtractor.py
@@ -1,5 +1,17 @@
 import tinytag as tt
 import eyed3
 
-def mdExtract(fileName):
-
+# extracts album art and other mp3 metadata
+def getMetadata(filename):
+    # get metadata
+    metadata = tt.TinyTag.get(filename)
+    # get album art
+    albumArt = metadata.get_image()
+    # get album name
+    album = metadata.album
+    # get artist name
+    artist = metadata.artist
+    # get song title
+    title = metadata.title
+    # get song length in seconds
+    length = metadata.duration
