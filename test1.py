15dd66e4e43d3ce83b39b644d20dbd7839f1e3f8
ewanpedersenUVM <143422879+ewanpedersenUVM@users.noreply.github.com>
2023-12-07 18:51:13 -0500

    boom

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@ import random
 
 wordlist = ["_", "_", "_", "_", "_"]
 
-def hangman(word, wordlist, decision, input, incorrect_guesses):
+"""def hangman(word, wordlist, decision, input, incorrect_guesses):
     splitWord = list(word)
     result = None
 
@@ -34,9 +34,48 @@ def hangman(word, wordlist, decision, input, incorrect_guesses):
 
         break  # Add a break statement to exit the loop
 
+    return incorrect_guesses, wordlist, result"""
+ 
+
+def hangman_letter(word, wordlist, input, incorrect_guesses):
+    splitWord = list(word)
+
+    while incorrect_guesses < 6:
+        letter_found = False
+        splitWord = list(word)
+        for index, letter in enumerate(splitWord):
+            if letter == input:
+                wordlist[index] = input
+                letter_found = True
+
+        if not letter_found:
+            incorrect_guesses += 1
+
+        if incorrect_guesses == 6:
+            result = "lose"
+            break
+        if input in word:
+            result = "yes"
+        if input in wordlist:
+            result = "repeat"
+
+        break  # Add a break statement to exit the loop
+
     return incorrect_guesses, wordlist, result
 
-result = hangman('apple', wordlist, 'letter', 'p', 0)
+result = hangman_letter('apple', wordlist, 'p', 0)
 print(result)
 print(wordlist)
- 
+
+def hangman_word(word, wordlist, input, incorrect_guesses):
+    while incorrect_guesses < 6:
+        if input == word:
+            wordlist = list(word)
+            result = "yes"
+        else:
+            incorrect_guesses += 1
+            result = "lose"
+
+        break  # Add a break statement to exit the loop
+
+    return incorrect_guesses, wordlist, result
