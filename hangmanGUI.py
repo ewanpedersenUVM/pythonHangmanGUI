8fe4c2c25cdae3c6b4db4f28adedc789cd99aa54
ewanpedersenUVM <143422879+ewanpedersenUVM@users.noreply.github.com>
2023-12-07 23:19:43 -0500

    final BOOM

--- hangmanGUI.py
+++ hangmanGUI.py
@@ -3,9 +3,10 @@ from PIL import Image, ImageTk
 from tkinter import messagebox
 import random
 
-global hiddenWord, wordlist, incorrect_guesses
-wordlist = []
 
+global hiddenWord, wordlist, incorrect_guesses, letterList
+wordlist = []
+letterList = []
 def get_number(x):
     # Replace the following line with your function to get a number between 0 and 6
     return x+1
@@ -17,68 +18,84 @@ def chooseWord():
         word = random.choice(words)
         wordList = ["_"] * len(word)
         return word, wordList
-
     
-def hangman_letter(word, wordlist, input, incorrect_guesses):
-    if len(input) != 1:
-        return incorrect_guesses, wordlist, False
-    splitWord = list(word)
+def leaderboard(name, incorrect_guesses):
+    with open("leaderboard.txt", "a") as file:
+        file.write(f"{name} | {incorrect_guesses}\n")
+
+#w = word
+#wl = wordlist
+#ip = input
+#incorrect_guesses = ig
+# we are using bad naming scheme because of global variables
+
+def hangman_letter(w, wl, ip, ig, ll):
+    global wordlist
+    if len(ip) != 1:
+        return ig, wl, "invalid",
+    splitWord = list(w)
+    result = None  # Initialize result variable outside the loop
+    letter_found = False
+    if ip in ll:
+      result = "repeat"
+    for index, letter in enumerate(splitWord):
+      if letter == ip:
+        wl[index] = ip
+        letter_found = True
+    if (not letter_found) and (result != "repeat"):
+        ig += 1
+        result = "no"
+    elif ip in w:
+        result = "yes"
+    elif ig == 6:
+        result = "lose"
+    if ip not in ll:
+      ll.append(ip)
+    return ig, wl, result, ll
+
+def hangman_word(w, wl, ip, ig):
+    global wordlist
     result = None  # Initialize result variable outside the loop
 
-    while incorrect_guesses < 6:
-        letter_found = False
-        for index, letter in enumerate(splitWord):
-            if letter == input:
-                wordlist[index] = input
-                letter_found = True
-        if not letter_found:
-            incorrect_guesses += 1
-
-        if incorrect_guesses == 6:
-            result = "lose"
-            break
-        if input in word:
-            result = "yes"
-        if input in wordlist:
-            result = "repeat"
-
-        break  # Add a break statement to exit the loop
-
-    return incorrect_guesses, wordlist, result
-
-def hangman_word(word, wordlist, input, incorrect_guesses):
-    result = None  # Initialize result variable outside the loop
-
-    while incorrect_guesses < 6:
-        if input == word:
-            wordlist = list(word)
+    while ig < 6:
+        if ip == w:
+            wl = list(w)
             result = "yes"
         else:
-            incorrect_guesses += 1
-            result = "lose"
+            ig += 1
+            result = "no"
 
         break  # Add a break statement to exit the loop
 
-    return incorrect_guesses, wordlist, result
+    return ig, wl, result
 
 class HangmanApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Hangman GUI")
-
+        # use aqua theme
+        self.root.tk_setPalette(background='#ececec')
+        # set header
+        self.root.geometry("500x500")
+        self.header = tk.Label(root, text="Welcome to Shamus and Ewans Hangman Game!")
+        self.header.pack()
+        
+        self.button0 = tk.Button(root, text="Start Game", command=self.button0_clicked)
+        self.button0.pack(side=tk.TOP, padx=5)
         # Text box
+        self.header = tk.Label(root, text="Enter a letter or word:")
+        self.header.pack()
         self.text_box = tk.Entry(root, width=30)
         self.text_box.pack(pady=10)
         # button to start game and choose a random word
         # Buttons to choose letter or word
-        self.button0 = tk.Button(root, text="Start Game", command=self.button0_clicked)
-        self.button0.pack(side=tk.TOP, padx=5)
+       
 
         self.button1 = tk.Button(root, text="Letter", command=self.button1_clicked)
-        self.button1.pack(side=tk.LEFT, padx=5)
+        self.button1.pack(side=tk.TOP, padx=5)
         
         self.button2 = tk.Button(root, text="Word", command=self.button2_clicked)
-        self.button2.pack(side=tk.RIGHT, padx=5)
+        self.button2.pack(side=tk.TOP, padx=5)
 
         # Hangman image
         self.image_path = "images/Hangman_0.png"
@@ -91,34 +108,71 @@ class HangmanApp:
         self.word_text = tk.Label(root, text="Word: " + " ".join(wordlist))
         self.word_text.pack()
 
+        # Text showing previous letter guesses
+        self.letter_text = tk.Label(root, text="Letters: " + " ".join(letterList))
+        self.letter_text.pack()
+
+        #leaderboard title, name input, and button to submit
+        self.leaderboard_title = tk.Label(root, text="Submit Name for Leaderboard")
+        self.leaderboard_title.pack()
+        self.leaderboard_name = tk.Entry(root, width=30)
+        self.leaderboard_name.pack(pady=10)
+        self.leaderboard_button = tk.Button(root, text="Submit", command=self.leaderboard_button_clicked)
+        self.leaderboard_button.pack(side=tk.TOP, padx=5)
+
+        #button to view leaderboard
+        self.leaderboard_button = tk.Button(root, text="View Leaderboard", command=self.leaderboardView_button_clicked)
+        self.leaderboard_button.pack(side=tk.TOP, padx=5)
+
     def button0_clicked(self): # start button
-        global hiddenWord, wordlist, incorrect_guesses
+        global hiddenWord, wordlist, incorrect_guesses, letterList
         hiddenWord, wordlist = chooseWord()
         incorrect_guesses = 0
+        letterList = []
         self.update_image(0)
         self.update_word_text(wordlist)
+        self.update_letter_text(letterList)
 
     def button1_clicked(self): # letter button
+        global hiddenWord, wordlist, incorrect_guesses, letterList
         input = str(self.text_box.get())
-        incorrect_guesses, wordlist, result = hangman_letter(hiddenWord, wordlist, input, incorrect_guesses)
+        incorrect_guesses, wordlist, result, letterList = hangman_letter(hiddenWord, wordlist, input, incorrect_guesses, letterList)
         if result == "yes":
             self.correct_message()
         elif result == "no":
             self.incorrect_message()
         elif result == "repeat":
             self.repeat_message()
-        self.update_image(int(incorrect_guesses))
-        self.update_word_text(wordlist)
-
-    def button2_clicked(self): # word button
-        input = str(self.text_box.get())
-        incorrect_guesses, wordlist, result = hangman_word(hiddenWord, wordlist, input, incorrect_guesses)
-        if result == "yes":
-            self.win_message()
         elif result == "lose":
             self.lose_message()
         self.update_image(int(incorrect_guesses))
         self.update_word_text(wordlist)
+        self.update_letter_text(letterList)
+
+    def button2_clicked(self): # word button
+        global hiddenWord, wordlist, incorrect_guesses, letterList
+        global name
+        input = str(self.text_box.get())
+        incorrect_guesses, wordlist, result = hangman_word(hiddenWord, wordlist, input, incorrect_guesses)
+        if result == "yes":
+            self.win_message()
+            leaderboard(name, incorrect_guesses)
+        elif result == "lose":
+            self.lose_message()
+        self.update_image(int(incorrect_guesses))
+        self.update_word_text(wordlist)
+
+    def leaderboard_button_clicked(self): # leaderboard button
+        global hiddenWord, wordlist, incorrect_guesses, letterList
+        global name
+        name = str(self.leaderboard_name.get())
+
+    def leaderboardView_button_clicked(self): # leaderboard view button
+        with open("leaderboard.txt", "r") as file:
+            allText = file.read()
+            self.leaderboard_text = tk.Label(root, text=allText)
+            self.leaderboard_text.pack()
+        
 
     def update_image(self, number):
         image_path = f"images/Hangman_{number}.png"
@@ -130,6 +184,10 @@ class HangmanApp:
 
     def update_word_text(self, wordlist):
         self.word_text.configure(text="Word: " + " ".join(wordlist))
+
+    # function to update text showing previous letter guesses
+    def update_letter_text(self, letterList):
+        self.letter_text.configure(text="Letters: " + " ".join(letterList))
 
     #message boxes for all outcomes: win, lose, repeat, incorrect guess
     def win_message(self):
@@ -150,7 +208,6 @@ class HangmanApp:
 
     def correct_message(self):
         messagebox.showinfo("Correct", "Correct letter!")
-
 
 if __name__ == "__main__":
     root = tk.Tk()
